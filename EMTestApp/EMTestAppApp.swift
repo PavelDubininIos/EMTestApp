846511057f9084ef9e85824093cc4ead5020bb5e
PavelDubinin <pavel.ios.develop@gmail.com>
2024-03-14 12:49:04 +0300

    Initial commit.

---
+++ EMTestApp/EMTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMTestAppApp.swift
+//  EMTestApp
+//
+//  Created by Павел Дубинин on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct EMTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
